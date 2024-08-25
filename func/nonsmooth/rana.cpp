97db6416eaa243951ca682a7efc83960dff287aa
Yufan Zheng <yfzheng@umd.edu>
2024-08-24 21:22:19 -0400

    add rana.cpp

--- func/nonsmooth/rana.cpp
+++ func/nonsmooth/rana.cpp
@@ -1,8 +1,8 @@
 #include <cmath>
+#include "potential.hpp"
 
 using namespace std;
 
-const double L = 3;
 const int dim = 2;
 
 /* parameters for encapsulation */
@@ -10,11 +10,7 @@ const double lb[] = {-500, -500};   // lower bound of the actual function range 
 const double ub[] = {500, 500};     // upper bound
 const double compress_coef = 0.95;  // the actual function will be compressed into a hypercube "bound": (compress_coef * [-L,L])^dim
 const double slope = 10;             // specifies how fast the encapsulated function will grow out of the "bound"
-
-void get_potential_params(double &var_L, int &var_dim) {
-    var_L = L;
-    var_dim = dim;
-}
+const double pinned[] = {-300.3376328023, 500}; // the pinned point will be guaranteed to be picked by the QHD discretization
 
 double get_obj(const double *x) {
     double x1 = x[0];
@@ -22,7 +18,7 @@ double get_obj(const double *x) {
 
     double t1 = sqrt(fabs(x2 + x1 + 1));
     double t2 = sqrt(fabs(x2 - x1 + 1));
-    return x1*sin(t2)*cos(t1) + (x2 + 1)*sin(t1)*cos(t2) + 500.736340;
+    return x1*sin(t2)*cos(t1) + (x2 + 1)*sin(t1)*cos(t2) + 500.802160296664;
 }
 
 void get_obj_subg(const double *x, double *ret) {
@@ -76,70 +72,5 @@ void get_obj_subg(const double *x, double *ret) {
                 (x1*cos(sqrt(-abs2))*cos(sqrt(-abs1))) / (2*sqrt(-abs2)) - 
                 (x2+1)*cos(sqrt(-abs2))*cos(sqrt(-abs1)) / (2*sqrt(-abs1)) + 
                 sin(sqrt(-abs1))*cos(sqrt(-abs2));
-    }
-}
-
-double get_potential(const double *x) {
-    double z[dim];                  // true coordinates to be mapped
-    double e[dim];                  // how far it is from outside the bound
-    double scale[dim];              // the scaling factor from x to z
-    double offset[dim];             // the offset factor from x to z
-    double func_val;
-
-    for (int i = 0; i < dim; i++) {
-        scale[i] = (ub[i] - lb[i]) / (2 * L * compress_coef);
-        offset[i] = (ub[i] + lb[i]) / 2;
-        z[i] = x[i] * scale[i] + offset[i];
-        if (z[i] > ub[i]) {
-            e[i] = z[i] - ub[i];
-            z[i] = ub[i];
-        } else if (z[i] < lb[i]) {
-            e[i] = z[i] - lb[i];
-            z[i] = lb[i];
-        } else {
-            e[i] = 0;
-        }
-    }
-
-    func_val = get_obj(z);
-    for (int i = 0; i < dim; i++) {
-        func_val += abs(e[i]) * slope;
-    }
-
-    return func_val;
-}
-
-int sgn(const double x) {
-    return (x > 0) - (x < 0);
-}
-
-void get_subgradient(const double *x, double *ret) {
-    double z[dim];                  // true coordinates to be mapped
-    double e[dim];                  // how far it is from outside the bound
-    double scale[dim];              // the scaling factor from x to z
-    double offset[dim];             // the offset factor from x to z
-    double func_subg[dim];
-
-    for (int i = 0; i < dim; i++) {
-        scale[i] = (ub[i] - lb[i]) / (2 * L * compress_coef);
-        offset[i] = (ub[i] + lb[i]) / 2;
-        z[i] = x[i] * scale[i] + offset[i];
-        if (z[i] > ub[i]) {
-            e[i] = z[i] - ub[i];
-            z[i] = ub[i];
-        } else if (z[i] < lb[i]) {
-            e[i] = z[i] - lb[i];
-            z[i] = lb[i];
-        } else {
-            e[i] = 0;
-        }
-    }
-
-    get_obj_subg(z, func_subg);
-    for (int i = 0; i < dim; i++) {
-        if (abs(e[i]) != 0) {
-            func_subg[i] = sgn(e[i]) * slope;
-        }
-        ret[i] = func_subg[i] * scale[i];
     }
 }
